39fb841c784cf3a570746b79bad7890b85a30f25
Abian Hernandez <abianhernandez@outlook.es>
2022-04-11 21:42:56 +0100

    New train function

--- derma/utils.py
+++ derma/utils.py
@@ -1,10 +1,37 @@
-import torch
+import torch, copy
 from tqdm import tqdm
 import numpy as np
+from .metric import Metrics
 
-def train(model, train_loader, optimizer, criterion, n_epoch, tb_writer, val_loader=None):
+
+def train(model, loader, optimizer, criterion, n_epoch, tb_writer) -> float:
+    '''
+    Train the model
+
+    Args:
+        model: nn.Module,
+            Model to train
+        
+        loader: DataLoader or list,
+            If list, then it should be a list of two DataLoader: train and validation loader.
+            It is necessary if you want to use validation set for a early stopping approach,
+            saving the model which best works for validation set.
+        
+        optimizer: optim.Optimizer,
+            Optimizer to use during training.
+            
+        criterion: loss function
+        n_epoch: number of epochs to train
+        tb_writer: tensorboard writer
+    '''
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = model.to(device) 
+    model = model.to(device)
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 1e10
+
+    train_loader, val_loader = loader if isinstance(loader, list) else (loader, None)
+    phase = ['train', 'eval'] if val_loader is not None else ['train']
 
     epoch_iterator = tqdm(
         range(n_epoch),
@@ -13,45 +40,47 @@ def train(model, train_loader, optimizer, criterion, n_epoch, tb_writer, val_loa
         postfix={"tls": "%.4f" % 1},
     )
 
-    # Metrics
-    loss_tr = []
-    total = 0
-    correct = 0
+    for epoch in epoch_iterator:
+        for p in phase:
+            model.train() if p == 'train' else model.eval()
+            loader = train_loader if p == 'train' else val_loader
+
+            epoch_acc_sum = 0.
+            epoch_loss_sum = 0.
+            for idx, (inputs, targets) in enumerate(loader):
+                optimizer.zero_grad()
+
+                inputs = inputs.to(device)
+                targets = targets.to(device)
+
+                with torch.set_grad_enabled(p == 'train'):
+                    outputs = model(inputs)
+                    loss = criterion(outputs, targets)
     
-    step=0
+                    epoch_loss_sum += loss.item()
+                    epoch_acc_sum += Metrics.accuracy(outputs, targets)
 
-    for _ in epoch_iterator:
-        for idx, (inputs, targets) in enumerate(train_loader):
-            model.train()
-            optimizer.zero_grad()
+                    if p == 'train':
+                        loss.backward()
+                        optimizer.step()
 
-            inputs = inputs.to(device)
-            targets = targets.to(device)
+                        if idx % 100 == 0:
+                            epoch_iterator.set_postfix(
+                                tls="%.4f" % (epoch_loss_sum/(idx+1)),
+                                acc="%.4f" % (epoch_acc_sum/(idx+1))
+                            )
 
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            loss_tr.append(loss.item())
+            epoch_loss = epoch_loss_sum/(idx+1)
+            epoch_acc = epoch_acc_sum/(idx+1)
+            tb_writer.add_scalar('{}/loss'.format(p), epoch_loss, epoch)
+            tb_writer.add_scalar('{}/accuracy'.format(p), epoch_acc, epoch)
 
-            loss.backward()
-            optimizer.step()
-
-            # Accuracy
-            predict = torch.argmax(outputs, 1)
-            total += targets.size(0)
-            correct += torch.eq(predict, targets).sum().double().item()
-
-            if idx % 150 == 0:
-                accuracy = correct/total
-
-                epoch_iterator.set_postfix(
-                    tls="%.4f" % np.mean(loss_tr),
-                    acc="%.4f" % accuracy
-                )
-                
-                tb_writer.add_scalar('train/loss', np.mean(loss_tr), step)
-                tb_writer.add_scalar('train/accuracy', accuracy, step)
-                step+=1
-
-                loss_tr = []
-                total = 0
-                correct = 0
+            if (p == 'eval') and (epoch_loss < best_loss):
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+                    
+    if val_loader is not None:
+        # load best model weights if validation was done
+        model.load_state_dict(best_model_wts)
+    
+    return best_loss
