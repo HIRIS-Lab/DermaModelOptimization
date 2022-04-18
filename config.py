c5e481f5b0fc2a4d102b274d3380476433f7d79c
Abián Hernández <abianhernandez@outlook.es>
2022-04-18 14:45:39 +0100

    More tests

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ r'''
     DATASET_DIR: Directorio donde se encuentran los datasets.
     RESULT_DIR: Directorio donde se guardan los resultados.
 '''
-DATASET_DIR = r'/home/abian/Data/Dataset/IUMA/Derma/0128-0128_dullrazor/'
-RESULT_DIR = r'/home/abian/Workspace/Thesis/IUMA/data/Derma'
+DATASET_DIR = r'/home/abian/Workspace/Dataset/Derma/HAM10000_balanced_000/'
+RESULT_DIR = r'/home/abian/Workspace/Projects/IUMA/data/Derma/'
